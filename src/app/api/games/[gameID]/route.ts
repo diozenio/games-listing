0e36457bb5b0b51cb676a3edba33498700e169c1
diozenio <d.dionisiobsl@gmail.com>
2023-10-28 11:09:37 -0300

    feat(api): Add /games/[gameID] endpoint

---
+++ src/app/api/games/[gameID]/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { gameID: string } }
+) {
+  try {
+    const res = await fetch(
+      `${process.env.API_URL}/games/${params.gameID}?key=${process.env.API_KEY}`
+    );
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (e) {
+    return NextResponse.json({ error: e });
+  }
+}

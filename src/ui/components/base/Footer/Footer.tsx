6cdecb1ffd5edcc69949b62d1f24493026fc1081
diozenio <d.dionisiobsl@gmail.com>
2023-10-28 11:28:02 -0300

    feat(components): Create Footer component

---
+++ src/ui/components/base/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="w-full py-8">
+      <div className="mx-auto w-fit text-[#3E4247] dark:text-[#EEEEF0] font-medium">
+        &copy; 2023 - Dionísio Barbosa
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
